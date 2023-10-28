062292ec097c61c317b72eadcb401592d4a85167
KPR111 <105378982+KPR111@users.noreply.github.com>
2023-10-28 11:06:20 +0530

    Create A. Garland_Easydp.cpp

---
+++ Codeforces/DP/A. Garland_Easydp.cpp
@@ -0,0 +1,184 @@
+//#define KPR_111
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+using ll= long long;
+typedef pair<ll, ll>          pll;
+typedef vector<ll>            vll;
+typedef vector<pll>           vpll;
+typedef vector<string>        vs;
+typedef unordered_map<ll,ll>  umll;
+typedef map<ll,ll>            mll;
+#define pb                    push_back
+#define eb                    emplace_back
+#define sz                    size()
+#define maxpq                 priority_queue<int>
+#define minpq                 priority_queue<int, vector<int>, greater<int> >
+#define f                     first
+#define s                     second
+#define vi                    vector<int>
+#define vvi                   vector<vi>
+#define vb                    vector<bool>
+#define pii                   pair<int,int>
+#define vpi                   vector<pii>
+#define vvb                   vector<vb>
+#define YES                   cout << "YES" << "\n"
+#define NO                    cout << "NO" << "\n"
+#define prDouble(x)           cout << fixed << setprecision(10) << x
+#define ispowoftwo(n)         (!(n & (n-1)))
+#define fo(i,m,n)             for(ll i=(ll)(m);i<(ll)(n);i++)
+#define rep(i,n)              fo(i,0,n)
+#define rl(i,n,m)             for(int i=n;i>=m;i--)
+#define all(v)                v.begin(),v.end()
+#define uniq(v)               (v).erase(unique(all(v)),(v).end()) //if you sort it and use you get uniqe elements
+#define mem(a, b)             memset(a, (b), sizeof(a))
+#define ct(x,v)               count(v.begin(),v.end(),x)
+#define mxe(v)                *max_element(v.begin(),v.end())
+#define mne(v)                *min_element(v.begin(),v.end())
+#define sum(v)                accumulate(all(v),0ll)
+#define endl                  "\n"
+#define inf                   (1LL<<30)
+
+
+#ifdef KPR_111
+#include "debug.h"
+#else
+#define deb(...)
+#endif
+
+
+const int N = 2e5 + 1;
+const int MOD = 1e9 + 7;
+ll fact[N], inv[N], invfact[N];
+void factInverse() {
+        fact[0] = inv[1] = fact[1] = invfact[0] = invfact[1] = 1;
+        for (long long i = 2; i < N; i++) {
+        fact[i] = (fact[i - 1] * i) % MOD;
+        inv[i] = MOD - (inv[MOD % i] * (MOD / i) % MOD);
+        invfact[i] = (inv[i] * invfact[i - 1]) % MOD;
+    }
+}
+
+
+ll mul(int x, int y) {
+    return (1LL * x * y) % MOD;
+}
+
+ll nCr(int n, int r) {
+        if (r > n)
+        return 0;
+    return mul(mul(fact[n], invfact[r]), invfact[n - r]);
+}
+
+
+const int Number = 1e6 + 5;
+vector<int> primes;
+bool prime[Number];
+void seive(){
+    mem(prime, true);
+    prime[0] = prime[1] = false;
+    for(int i = 2; i * i <= Number; i++){
+        if(prime[i]){
+            for(int j = i * i; j <= Number; j += i){
+                prime[j] = false;
+            }
+        }
+    }
+    for(int i = 2; i <= Number; i++){
+        if(prime[i]) primes.pb(i);
+    }
+}
+
+int binpow(int x, int y)
+{
+    int z = 1;
+    while(y)
+    {
+        if(y & 1) z = mul(z, x);
+        x = mul(x, x);
+        y >>= 1;
+    }
+    return z;
+}
+
+int n;
+vi v;
+int odd,even;
+
+int dp[101][101][101][2];
+
+int solve(int i,int odd,int even,int last){
+
+    if(i>=n && odd==0 && even==0){
+        return 0;
+    }
+    if(odd<0 || even<0)return 1e9;
+
+    int &ans=dp[i][odd][even][last];
+    if(~ans)
+        return ans;
+    ans=1e9;
+
+    if(v[i]!=0){
+        ans= (v[i]%2!=last?1:0)+solve(i+1,odd,even,v[i]%2);
+    }
+    else{
+
+        if(last){
+            ans=min(ans,solve(i+1,odd-1,even,1));
+            ans=min(ans,1+solve(i+1,odd,even-1,0));
+        }
+        else{
+            ans=min(ans,1+solve(i+1,odd-1,even,1));
+            ans=min(ans,solve(i+1,odd,even-1,0));
+        }
+    }
+
+    return ans;
+
+}
+
+void solve(){
+    mem(dp,-1);
+    cin>>n;
+    v.resize(n);
+    rep(i,n)cin>>v[i];
+    vi a;
+
+    rep(i,n){
+        if(v[i]==0)continue;
+        if(v[i]%2)odd++;
+        else even++;
+    }
+    if(n%2){
+        odd=(n/2+1)-odd;
+        even=n/2-even;
+    }else{
+        odd=n/2-odd;
+        even=n/2-even;
+    }
+
+    if(v[0]==0){
+        cout<<min(solve(1,odd-1,even,1),solve(1,odd,even-1,0));
+    }
+    else cout<<solve(1,odd,even,v[0]%2);
+    
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt = 1;
+    // cin >> tt;
+    for(int t = 0; t < tt; t++) {
+        //cout << "Case #" << t+1 << ": ";
+        solve();
+        //cout << endl;
+        #ifdef KPR_111
+            cout << "__________________________" << endl;
+        #endif
+    }
+}
